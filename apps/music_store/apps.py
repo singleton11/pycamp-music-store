01cec8aaa3bcbf42604975efa2a9b67b171ce79d
DenisDontsov <de-don@mail.ru>
2018-04-03 11:06:28 +0700

    Init new app and create necessary files to app.

---
+++ apps/music_store/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class MusicStoreAppDefaultConfig(AppConfig):
+    """Default configuration for MusicStore app
+    """
+
+    name = 'apps.music_store'
+    verbose_name = 'MusicStore'
