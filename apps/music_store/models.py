11da01b1d8372cdda65cbaedc944d8b5f18a2920
Denis Dontsov <de-don@mail.ru>
2018-04-04 10:48:35 +0700

    Create PaymentAccount, PaymentMethod and BoughtTrack.
    The model 'StoreUser' is deleted, instead of it created the model 'PaymentAccount'.

--- apps/music_store/models.py
+++ apps/music_store/models.py
@@ -5,18 +5,47 @@ AppUser = get_user_model()
 
 
 class Track:
+    """Blank model for Track"""
     pass
 
 
 class Album:
+    """Blank model for Album"""
     pass
 
 
-class StoreUser(models.Model):
+class PaymentMethod(models.Model):
+    """Model to store payment methods.
+
+    User can select method Only admin can create methods.
+    """
+    title = models.CharField(max_length=100)
+
+
+class PaymentAccount(models.Model):
+    """Payment account for each user
+
+    Attributes:
+        user(AppUser): owner of this payment account.
+        balance(float): current balance.
+        payment_methods(PaymentMethod[]): saved payment methods
+        default_payment_methods(PaymentMethod): default payment method
+    """
     user = models.OneToOneField(AppUser)
     balance = models.FloatField(default=0.0)
 
+    payment_methods = models.ManyToManyField(PaymentMethod)
+    default_payment_methods = models.ForeignKey(PaymentMethod)
+
 
 class BoughtTrack(models.Model):
+    """Model for storing a bought track after purchase
+
+    Attributes:
+        user(AppUser): user who bought the track
+        track(AppUser): track purchased by the user
+        date_purchase(DateTimeField): date of purchase
+    """
+    user = models.ForeignKey(AppUser)
     track = models.ForeignKey(Track)
     date_purchase = models.DateTimeField()
