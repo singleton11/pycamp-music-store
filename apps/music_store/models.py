634365addc3efde9064ca5bcea3c9c0a56f903d1
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-04-05 17:50:02 +0700

    Add django.extensions to models

--- apps/music_store/models.py
+++ apps/music_store/models.py
@@ -1,55 +1,61 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-from apps.users.models import AppUser
+from django_extensions.db.models import TimeStampedModel, TitleDescriptionModel
+
+from ..users.models import AppUser
 
 
-class Album(models.Model):
+class Album(
+    TitleDescriptionModel,
+    TimeStampedModel,
+    models.Model,
+):
     """Music album with its title, image, price and related tracks.
 
     Attributes:
         title (CharField): text representation of album's title
         image (Charfield): text representation of URL to album's image
         price (FloatField): price of album. Minimal price is 0
-        created (DateTimeField): date of creation. Adds automatically
-
     """
-    title = models.CharField(verbose_name='Title', max_length=200)
-    image = models.CharField(verbose_name='Image', max_length=200)
+    image = models.CharField(
+        verbose_name=_('Image'),
+        max_length=200
+    )
     price = models.FloatField(
-        verbose_name='Price',
+        verbose_name=_('Price'),
         validators=[MinValueValidator(0.0)]
     )
 
-    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return self.title
 
     @property
     def is_empty(self):
         """bool: True if no related Tracks"""
         return not self.tracks.exists()
 
-    class Meta:
-        ordering = ('created',)
-
     def buy_album(self, user):
         """Method to buy music track"""
         pass
 
-    def __str__(self):
-        return self.title
 
-
-class Track(models.Model):
+class Track(
+    TitleDescriptionModel,
+    TimeStampedModel,
+    models.Model,
+):
     """Music track with its title, price and album if exists.
 
     Attributes:
         title (CharField): text representation of track's title
         album (Album): album that contains the track
         price (FloatField): price of album. Minimal price is 0
-        created (DateTimeField): date of creation. Adds automatically
-
     """
-    title = models.CharField(verbose_name='Title', max_length=200)
     album = models.ForeignKey(
         Album,
         verbose_name='Album',
@@ -62,13 +68,11 @@ class Track(models.Model):
         validators=[MinValueValidator(0.0)]
     )
 
-    # free_version
-    # full_version
-
-    created = models.DateTimeField(auto_now_add=True)
-
     class Meta:
         ordering = ('created',)
+
+    def __str__(self):
+        return self.title
 
     def set_like(self, user):
         """Method to put like on track"""
@@ -86,27 +90,29 @@ class Track(models.Model):
         """Method to buy music track"""
         pass
 
-    def __str__(self):
-        return self.title
 
-
-class LikeTrack(models.Model):
+class LikeTrack(
+    TimeStampedModel,
+    models.Model,
+):
     """A 'Like' to music track.
 
-    Unique pair of user who likes track and track, that is liked by user."""
+    Unique pair of user who likes track and track, that is liked by user.
+    """
+    track = models.ForeignKey(Track)
+    user = models.ForeignKey(AppUser)
+
     class Meta:
         unique_together = (('track', 'user'),)
 
-    track = models.ForeignKey(Track)
-    user = models.ForeignKey(AppUser)
-    like_time = models.DateTimeField(auto_now_add=True)
 
-
-class ListenTrack(models.Model):
+class ListenTrack(
+    TimeStampedModel,
+    models.Model
+):
     """A note about each listening of any track by any user.
 
-    Each track may be listened multiple times."""
+    Each track may be listened multiple times.
+    """
     track = models.ForeignKey(Track)
     user = models.ForeignKey(AppUser)
-    listen_time = models.DateTimeField(auto_now_add=True)
-
