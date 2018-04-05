153ef2cb38f69345de0f30e2b6dc21a7afbcb280
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-04-05 17:49:25 +0700

    Code style fixes

--- apps/music_store/api/urls.py
+++ apps/music_store/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 
 from rest_framework.routers import DefaultRouter
 
-from apps.music_store.api import views
+from ...music_store.api import views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
--- apps/music_store/api/views.py
+++ apps/music_store/api/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
 
-from apps.music_store.api.serializers.music_albums_tracks import (
+from ...music_store.api.serializers.serializer import (
     AlbumSerializer,
     LikeTrackSerializer,
     ListenTrackSerializer,
     TrackSerializer,
 )
-from apps.music_store.models import Album, LikeTrack, ListenTrack, Track
+from ...music_store.models import Album, LikeTrack, ListenTrack, Track
 
 # ##############################################################################
 # MUSIC ALBUMS
@@ -14,9 +14,7 @@ from apps.music_store.models import Album, LikeTrack, ListenTrack, Track
 
 
 class AlbumViewSet(viewsets.ModelViewSet):
-    """List all music albums, or create a new album.
-
-    """
+    """List all music albums, or create a new album."""
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
@@ -29,9 +27,7 @@ class AlbumViewSet(viewsets.ModelViewSet):
 
 
 class TrackViewSet(viewsets.ModelViewSet):
-    """List all music tracks, or create a new track.
-
-    """
+    """List all music tracks, or create a new track."""
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
 
@@ -44,22 +40,22 @@ class TrackViewSet(viewsets.ModelViewSet):
 
 
 class LikeTrackViewSet(viewsets.ModelViewSet):
-    """List likes for all music tracks and users.
-
-    """
+    """List likes for all music tracks and users."""
     queryset = LikeTrack.objects.all()
     serializer_class = LikeTrackSerializer
 
     def perform_create(self, serializer):
         """Put a Like to some track.
 
-        Allows to save Like only to logged user"""
+        Allows to save Like only to logged user
+        """
         serializer.save(user=self.request.user)
 
     def perform_destroy(self, instance):
         """Put away Like from the track.
 
-        Allows to destroy Like only to user who put that like"""
+        Allows to destroy Like only to user who put that like
+        """
         if self.request.user == instance.user:
             instance.delete()
 
@@ -73,9 +69,7 @@ class LikeTrackViewSet(viewsets.ModelViewSet):
 
 
 class ListenTrackViewSet(viewsets.ModelViewSet):
-    """List all listens of all tracks by current user.
-
-    """
+    """List all listens of all tracks by current user."""
     queryset = ListenTrack.objects.all()
     serializer_class = ListenTrackSerializer
 
@@ -84,7 +78,8 @@ class ListenTrackViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Put a Like to some track.
 
-        Allows to save Like only to logged user"""
+        Allows to save Like only to logged user
+        """
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
