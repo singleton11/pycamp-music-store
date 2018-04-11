7d6f0adb3694c23b68dbed58741576935557a1d0
Denis Dontsov <de-don@mail.ru>
2018-04-11 10:52:21 +0700

    Add factory for BoughtAlbum

--- apps/music_store/factories.py
+++ apps/music_store/factories.py
@@ -1,13 +1,12 @@
 import factory
 from factory import fuzzy
 
+from apps.music_store.models import BoughtAlbum
 from apps.users.factories import UserWithBalanceFactory
-
 from .models import Album, BoughtTrack, Track
 
 
 class AlbumFactory(factory.django.DjangoModelFactory):
-
     title = factory.Faker('sentence', nb_words=2)
     image = factory.Faker('sentence', nb_words=2)
     price = factory.fuzzy.FuzzyInteger(0, 50)
@@ -17,7 +16,6 @@ class AlbumFactory(factory.django.DjangoModelFactory):
 
 
 class TrackFactory(factory.django.DjangoModelFactory):
-
     title = factory.Faker('sentence', nb_words=2)
     price = factory.fuzzy.FuzzyInteger(0, 50)
     album = factory.SubFactory(AlbumFactory)
@@ -48,3 +46,19 @@ class BoughtTrackFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = BoughtTrack
+
+
+class BoughtAlbumFactory(factory.DjangoModelFactory):
+    """Factory for generates test Track model with random price and title """
+
+    item = factory.SubFactory(
+        AlbumFactory,
+        price=fuzzy.FuzzyInteger(1, 10),
+    )
+    user = factory.SubFactory(
+        UserWithBalanceFactory,
+        balance=fuzzy.FuzzyInteger(11, 20)
+    )
+
+    class Meta:
+        model = BoughtAlbum
