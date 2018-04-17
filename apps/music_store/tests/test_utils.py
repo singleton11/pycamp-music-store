3f80442cc727a18742fcea3f0dd3c927b192471c
fr0mhell <t1m0feev.grigorij@gmail.com>
2018-04-17 23:28:22 +0700

    Add tests for zip archive handler

--- apps/music_store/tests/test_utils.py
+++ apps/music_store/tests/test_utils.py
@@ -1,39 +1,29 @@
-from apps.music_store.utils import handle_uploaded_archive, NestedDirectoryError
+from apps.music_store.utils import (
+    handle_uploaded_archive,
+    NestedDirectoryError,
+    AlbumUploader,
+)
+from apps.music_store.models import Track, Album
 from unittest.mock import patch, Mock, mock_open
 from django.test import TestCase
 from faker import Faker
-import builtins
 from zipfile import ZipFile
 
 
 fake = Faker()
-mock_openfile = mock_open(read_data=fake.sentence(20))
+mock_openfile = mock_open(read_data=fake.sentence(30))
 
 
-def mock_infolist(albums=False, nested=False):
-    """Mock of Zipfile.infolist() method
-
-    Args:
-        albums (bool): if mock zip provide album directories
-        nested (bool): of any album dir contain nested dir
+def mock_infolist():
+    """Mock of Zipfile.infolist() method.
 
     """
     files = [
         Mock(filename=fake.file_name()),
-        Mock(filename=f'{fake.word} - {fake.file_name()}'),
+        Mock(filename=f'{fake.word()} - {fake.file_name()}'),
+        Mock(filename=f'{fake.word()}/{fake.file_name()}'),
+        Mock(filename=f'{fake.word()} - {fake.word()}/{fake.file_name()}'),
     ]
-    if albums:
-        files.extend(
-            [
-                Mock(filename=f'{fake.word} - {fake.word}/{fake.file_name()}'),
-                Mock(filename=f'{fake.word}/{fake.file_name()}'),
-            ]
-        )
-    if nested:
-        files.append(
-            Mock(filename=fake.file_path(2))
-        )
-
     return files
 
 
@@ -43,6 +33,10 @@ class TestUploadZIPArchive(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.archive = './apps/music_store/tests/test.zip'
+        cls.handler = AlbumUploader()
+        cls.track_title = fake.word()
+        cls.album_title = fake.word()
+        cls.author = fake.word()
 
     @patch('zipfile.is_zipfile', return_value=False)
     def test_not_a_zip_file(self, mock_iszip):
@@ -51,11 +45,70 @@ class TestUploadZIPArchive(TestCase):
         with self.assertRaises(TypeError):
             handle_uploaded_archive(self.archive)
 
-    @patch('apps.music_store.utils.add_track', side_effect=NestedDirectoryError)
+    @patch.object(AlbumUploader, 'no_nested_folders_in_albums', return_value=False)
     def test_archive_with_nested_directories(self, mock_add_track):
         """Test for nested directories in album folder"""
 
         with self.assertRaises(NestedDirectoryError):
             handle_uploaded_archive(self.archive)
+
+    def test_data_from_filename_filename_with_only_track_title(self):
+        filename = f'{self.track_title}'
+        self.assertEqual(
+            self.handler._get_data_from_filename(filename),
+            {
+                'author': None,
+                'album': None,
+                'track': self.track_title
+            }
+        )
+
+    def test_data_from_filename_filename_with_author_and_track_title(self):
+        filename = f'{self.author} - {self.track_title}'
+        self.assertEqual(
+            self.handler._get_data_from_filename(filename),
+            {
+                'author': self.author,
+                'album': None,
+                'track': self.track_title
+            }
+        )
+
+    def test_data_from_filename_filename_with_album_and_track_title(self):
+        filename = f'{self.album_title}/{self.track_title}'
+        self.assertEqual(
+            self.handler._get_data_from_filename(filename),
+            {
+                'author': None,
+                'album': self.album_title,
+                'track': self.track_title
+            }
+        )
+
+    def test_data_from_filename_filename_full_info(self):
+        filename = f'{self.author} - {self.album_title}/{self.track_title}'
+        self.assertEqual(
+            self.handler._get_data_from_filename(filename),
+            {
+                'author': self.author,
+                'album': self.album_title,
+                'track': self.track_title
+            }
+        )
+
+    @patch.object(ZipFile, 'infolist()', mock_infolist)
+    @patch('builtins.open', mock_openfile)
+    def test_zip_album_handler(self):
+        self.handler.zip_album_handler(self.archive)
+
+        self.assertEqual(
+            Track.objects.all().count(),
+            4
+        )
+
+        self.assertEqual(
+            Album.objects.all().count(),
+            2
+        )
 
 
