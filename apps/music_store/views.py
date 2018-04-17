e13b12268bce8a849e61323cb32a6642f2867674
Denis Dontsov <de-don@mail.ru>
2018-04-17 14:58:23 +0700

    Add `title` to context of upload archive view

--- apps/music_store/views.py
+++ apps/music_store/views.py
@@ -24,5 +24,5 @@ class AlbumUploadArchiveView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Album Upload'
+        context['title'] = 'Albums Upload'
         return context
