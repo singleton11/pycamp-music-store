b5a7c10a505ca3fbff547a58d1c441fc297880c9
GrigoryTimofeev <GrigoryTimofeev@saritasa.com>
2018-04-20 15:13:21 +0700

    TaskStatusView display data about celery task in json format.

--- apps/music_store/views.py
+++ apps/music_store/views.py
@@ -1,10 +1,13 @@
 from django.views.generic import FormView
+from django.views import View
+from django.http import HttpResponse
 
 from apps.music_store.forms import AlbumUploadArchiveForm
 
 from django.core.files.storage import default_storage
 from .tasks import get_albums_from_zip
 import uuid
+import json
 from config.celery import app
 
 
@@ -36,3 +39,20 @@ class AlbumUploadArchiveView(FormView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Albums Upload'
         return context
+
+
+class TaskStatusView(View):
+    """View for tracking status of uploading tasks."""
+    def get(self, request, task_id, *args, **kwargs):
+        """"""
+        task_id = kwargs.get('task_key')
+        task_status = app.AsyncResult(task_id).state
+        task_result = app.AsyncResult(task_id).result
+
+        task_data = {
+            'task_id': task_id,
+            'task_status': task_status,
+            'task_result': task_result
+        }
+
+        return HttpResponse(json.dumps(task_data), mimetype='application/json')
