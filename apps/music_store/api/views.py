d59e06c45f65b64ec2f1ebd33ec06dfdecd0db9f
Denis Dontsov <de-don@mail.ru>
2018-05-14 08:56:00 +0700

    Fixed docstrings

--- apps/music_store/api/views.py
+++ apps/music_store/api/views.py
@@ -36,7 +36,7 @@ from ...music_store.models import (
 
 
 class ItemsPagination(PageNumberPagination):
-    """Pagination for payment transactions"""
+    """Pagination for lists of items"""
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
